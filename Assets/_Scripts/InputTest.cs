eb4ebea7d4c3ab9f45d13222c920dd0df49d45e3
Alizar <zhagipparov.a@nisa.edu.kz>
2026-02-07 17:58:07 +0500

    3

---
+++ Assets/_Scripts/InputTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    private void Update()
+    {
+        if (InputManager.Instance == null)
+        {
+            Debug.LogWarning("InputManager íå ñîçäàí!");
+            return;
+        }
+
+        Vector2 move = InputManager.Instance.MoveInput;
+        Vector2 look = InputManager.Instance.LookInput;
+
+        if (move != Vector2.zero)
+            Debug.Log($"Move: {move}");
+
+        if (look != Vector2.zero)
+            Debug.Log($"Look: {look}");
+
+        if (InputManager.Instance.JumpPressed)
+            Debug.Log("Jump pressed!");
+
+        if (InputManager.Instance.AttackPressed)
+            Debug.Log("Attack pressed!");
+
+        // Ñáðîñ ôëàãîâ
+        InputManager.Instance.ResetButtonFlags();
+    }
+}
